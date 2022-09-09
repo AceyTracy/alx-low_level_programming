0c644bf1c3d8e0fbb3c426756dca0446f493fc41
AceyTracy <werutracy@gmail.com>
2022-09-09 12:42:02 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,23 +8,24 @@
 int main(void)
 {
 	int c, i;
-
+          
 	c = i = '0';
-
+                           
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
 			putchar(c);
 			putchar(i);
-
-			if ((c != '9') || (c == '9' && i != '9'))
+                                
+			if (c != '8' || (c == '9' && i != '9'))
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+}
 
 	putchar('\n');
 
