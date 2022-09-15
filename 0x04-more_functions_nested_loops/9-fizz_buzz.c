942f9c8876cfa4c4a5a4834c7b7eb5184f6bfec5
AceyTracy <werutracy@gmail.com>
2022-09-15 13:52:40 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * print_square - Prints n squares according n number of times
- * @size: The number of squares/number of times
+/*
+ * main - Prints a Fizz Buzz program
  *
- * Return: empty
+ * Return: Always 0 (Success)
  */
-void print_square(int size)
+int main(void)
 {
-int x, y;
-
-if (size <= 0)
+int x;
+for (x = 1; x <= 100; x++)
 {
-_putchar('\n');
+if  ((x % 3 == 0) && (x % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (x % 3 == 0)
+{
+printf("Fizz");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz");
 }
 else
 {
-for (x = 0; x < size; x++)
-{
-for (y = 0; y < size; y++)
-{
-_putchar(35);
+printf("%d", x);
 }
+if (x != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
 
-_putchar('\n');
-}
-}
+return (0);
 }
